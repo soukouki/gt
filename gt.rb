d13693906e7639fde888dcc468329e3c8f4b0f1f
soukouki <soukouki0@yahoo.co.jp>
2016-11-15 20:03:00 +0900

    merge --ffを追加
    ソースまとめないと・・・

--- gt.rb
+++ gt.rb
@@ -3,7 +3,9 @@
 def command command_name, argument
 	abort "#{command_name}の引数がありません。" if argument.include?(nil)
 	# Array#shiftなどは、要素が無いときはnilを返すのでそれを見つける
-	puts `git #{command_name} #{argument.map{|a| %!"#{a}"! }.join(" ")}`
+	command = "git #{command_name} #{argument.map{|a| %!"#{a}"! }.join(" ")}"
+	puts command
+	puts `#{command}`
 	return_num = $?.to_i
 	abort "#{command_name}に失敗しました。" unless return_num==0
 end
@@ -29,6 +31,13 @@ def merge input, target
 	end
 end
 
+def merge_ff input, target
+	if input.take(2)==["m", "f"]
+		input.shift(2)
+		command "merge --ff", [target]
+	end
+end
+
 def checkout input, terget
 	if input[0]=="c"
 		input.shift
@@ -49,18 +58,21 @@ sync input
 if input[0]=="c"
 	start_branch = current_branch
 	checkout(input, ARGV.shift)
-
+	
+	merge_ff(input, start_branch)
 	merge(input, start_branch)
-
-	delete_branch input, start_branch
-
+	
+	delete_branch(input, start_branch)
+	
 	sync input
-
+	
 	checkout(input, start_branch)
 elsif input[0]=="m"
 	merge_target = ARGV.shift
+	
+	merge_ff(input, merge_target)
 	merge(input, merge_target)
-
+	
 	delete_branch(input, merge_target)
 elsif input[0]=="d"
 	delete_branch(input, ARGV.shift)
