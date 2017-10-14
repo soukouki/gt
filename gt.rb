d5af847e62b3b5ac23c64ffec11d87ed61ba6755
soukouki <soukouki0@yahoo.co.jp>
2017-10-15 02:44:00 +0900

    nbを実装

--- gt.rb
+++ gt.rb
@@ -9,6 +9,7 @@ end
 # コマンド群
 module Cmd extend self
 	# 処理の流れにコマンドと省略コマンドを入れる
+	# 戻り値に、そのコマンドを実行したかどうかが入る
 	def sync commands
 		commands_base("sync", "s", commands, [])
 	end
@@ -28,18 +29,18 @@ module Cmd extend self
 		commands_base("checkout", "c", commands, [target])
 	end
 	def new_branch commands, target
-		commands_base("checkout -b", commands, [])
+		commands_base("checkout -b", "nb", commands, [target])
 	end
 	
 	private
 	# コマンドがあっていた場合、その分を削り、コマンドを実行する
-	def commands_base name, abbreviation_command, commands, target
+	def commands_base name, abbreviation_command, commands, argument
 		# nilはshiftでの要素が足りないときの戻り値
-		abort "ターゲットが足りません。コマンド:'#{name}' 省略コマンド:'#{abbreviation_command}' ターゲット:'#{target}'" if target.include?(nil)
+		abort "ターゲットが足りません。コマンド:'#{name}' 省略コマンド:'#{abbreviation_command}' ターゲット:'#{argument}'" if argument.include?(nil)
 		abbcom_len = abbreviation_command.length
 		if commands.take(abbcom_len).join("")==abbreviation_command
 			commands.shift(abbcom_len)
-			exec_command(name, target)
+			exec_command(name, argument)
 			return true
 		end
 		return false
@@ -59,48 +60,48 @@ def current_branch
 end
 
 # この関数群を動き回って処理していく
-def start commands, target
+def start commands, targets
 	while commands.length > 0
 		case
-		when commands[0..1].join("") == "nb"
-			p :nb
-			exit
-		when commands[0] == "c"
-			checkouts(commands, target)
+		when commands[0..1].join("") == "nb" || commands[0] == "c"
+			start_branch = current_branch
+			target_branch = targets.shift
+			# どちらか一方だけ実行するため
+			Cmd.new_branch(commands, target_branch) || Cmd.checkout(commands, target_branch)
+			checkout_after(commands, start_branch)
 		when commands[0] == "m"
-			merges(commands, target)
+			merges(commands, targets)
 		when commands[0] == "s"
 			Cmd.sync(commands)
 		when commands[0] == "d"
-			Cmd.delete_branch(commands, target.shift)
+			Cmd.delete_branch(commands, targets.shift)
 		when commands[0] == "-"
 			commands.shift
 		else
 			abort "コマンドが不明です'#{commands.join}'"
 		end
 	end
-	"処理が終了した際にターゲットが残っています。'#{target}'" if target.length > 0
+	"処理が終了した際にターゲットが残っています。'#{targets}'" if targets.length > 0
 end
-# チェックアウトの後にいろいろする場合
-def checkouts commands, target
-	start_branch = current_branch
-	
-	Cmd.checkout(commands, target.shift)
-	Cmd.merge_ff(commands, start_branch)
-	Cmd.merge_squash(commands, start_branch)
-	Cmd.merge(commands, start_branch)
-	is_deleted = Cmd.delete_branch(commands, start_branch)
-	Cmd.sync commands
-	Cmd.checkout(commands, start_branch) unless is_deleted
+# チェックアウトの後などに、チェックアウト前に対してターゲットを省略するもの
+def checkout_after commands, start_branch
+	merge3(commands, start_branch)
+	Cmd.delete_branch(commands, start_branch)
+	Cmd.checkout(commands, start_branch) # どちらかしかできないが、どうせエラーでわかるのでそのまま
 end
-# マージの後にいろいろする場合
-def merges commands, target
-	merge_target = target.shift
+# マージの際、マージ先に対してのコマンドで、ターゲットを省略するもの
+# 戻り値に、ブランチを削除したかどうかを返す
+def merges commands, targets
+	merge_target = targets.shift
 	
-	Cmd.merge_ff(commands, merge_target)
-	Cmd.merge_squash(commands, merge_target)
-	Cmd.merge(commands, merge_target)
-	Cmd.delete_branch(commands, merge_target)
+	merge3(commands, merge_target)
+	Cmd.delete_branch(commands, merge_targets)
+end
+# 三種類のマージを試す。
+def merge3 commands, target
+	Cmd.merge_ff(commands, target)
+	Cmd.merge_squash(commands, target)
+	Cmd.merge(commands, target)
 end
 
 # 起動用
