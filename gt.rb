cd03f0a1d96c08e1d2b4b522156a4a94928015b3
soukouki <soukouki0@yahoo.co.jp>
2017-10-15 01:44:45 +0900

    startの無駄な再帰をループに変更

--- gt.rb
+++ gt.rb
@@ -7,46 +7,50 @@ def command cmd
 end
 
 # コマンド群
-module Cmd
-	class << self
-		def sync input
-			commands_base("sync", "s", input, [])
+module Cmd extend self
+	# 処理の流れにコマンドと省略コマンドを入れる
+	def sync input
+		commands_base("sync", "s", input, [])
+	end
+	def delete_branch input, target
+		commands_base("branch -D", "d", input, [target])
+	end
+	def merge input, target
+		commands_base("merge --no-ff", "m", input, [target])
+	end
+	def merge_ff input, target
+		commands_base("merge --ff", "mf", input, [target])
+	end
+	def merge_squash input, target
+		commands_base("merge --squash", "ms", input, [target])
+	end
+	def checkout input, target
+		commands_base("checkout", "c", input, [target])
+	end
+	def new_branch input, target
+		commands_base("checkout -b", input, [])
+	end
+	
+	private
+	# ちょっとよくわかんない
+	def commands_base name, abbreviation_command, input, target
+		abbcommand_length = abbreviation_command.length
+		if input.take(abbcommand_length).join("")==abbreviation_command
+			input.shift(abbcommand_length)
+			exec_command(name, target)
+			return true
 		end
-		def delete_branch input, target
-			commands_base("branch -D", "d", input, [target])
-		end
-		def merge input, target
-			commands_base("merge", "m", input, [target])
-		end
-		def merge_ff input, target
-			commands_base("merge --ff", "mf", input, [target])
-		end
-		def merge_squash input, target
-			commands_base("merge --squash", "ms", input, [target])
-		end
-		def checkout input, target
-			commands_base("checkout", "c", input, [target])
-		end
-		
-		private
-		def commands_base name, abbreviation_command, input, target
-			abbcommand_length = abbreviation_command.length
-			if input.take(abbcommand_length).join("")==abbreviation_command
-				input.shift(abbcommand_length)
-				exec_command(name, target)
-				return true
-			end
-			return false
-		end
-		def exec_command name, argument
-			abort "#{name}の引数がありません。" if argument.include?(nil)
-			# Array#shiftなどは、要素が無いときはnilを返すのでそれを見つける
-			exe_shell = "git #{name} #{argument.map{|a| %!"#{a}"! }.join(" ")}"
-			puts exe_shell
-			puts command(exe_shell)
-			return_num = $?.to_i
-			abort "#{name}に失敗しました。" unless return_num==0
-		end
+		return false
+	end
+	#
+	def exec_command name, argument
+		abort "#{name}の引数がありません。" if argument.include?(nil)
+		# Array#shiftなどは、要素が無いときはnilを返すのでそれを見つける
+		exe_shell = "git #{name} #{argument.map{|a| %!"#{a}"! }.join(" ")}"
+		puts "\n"+exe_shell
+		puts "\t"+command(exe_shell)
+		return_num = $?.to_i
+		abort "#{name}に失敗しました。" unless return_num==0
 	end
 end
 
@@ -55,25 +59,29 @@ def current_branch
 end
 
 # この関数群を動き回って処理していく
-def start input, target
-	case input[0]
-	when "c"
-		checkouts(input, target)
-	when "m"
-		merges(input, target)
-	when "s"
-		Cmd.sync(input)
-	when "d"
-		Cmd.delete_branch(input, target.shift)
-	when "-"
-		input.shift
-	else
-		abort "コマンドが不明です'#{input.join}'"
+def start commands, target
+	while commands.length > 0
+		case
+		when commands[0..1].join("") == "nb"
+			p :nb
+			exit
+		when commands[0] == "c"
+			checkouts(commands, target)
+		when commands[0] == "m"
+			merges(commands, target)
+		when commands[0] == "s"
+			Cmd.sync(commands)
+		when commands[0] == "d"
+			Cmd.delete_branch(commands, target.shift)
+		when commands[0] == "-"
+			commands.shift
+		else
+			abort "コマンドが不明です'#{commands.join}'"
+		end
 	end
-	if input.length>0
-		start(input, target)
-	end
+	"処理が終了した際にターゲットが残っています。'#{target}'" if target.length > 0
 end
+# チェックアウトの後にいろいろする場合
 def checkouts input, target
 	start_branch = current_branch
 	
@@ -85,6 +93,7 @@ def checkouts input, target
 	Cmd.sync input
 	Cmd.checkout(input, start_branch) unless is_deleted
 end
+# マージの後にいろいろする場合
 def merges input, target
 	merge_target = target.shift
 	
@@ -97,4 +106,3 @@ end
 # 起動用
 input = ARGV.shift.split("")
 start(input, ARGV)
-puts("処理が終了した際に引数が残っています'#{ARGV}'") unless ARGV.empty?
